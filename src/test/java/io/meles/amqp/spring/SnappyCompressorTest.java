afb72f69f7c988da5bad36646265ecdeefbef4dd
Neil Green <neil@melessoftware.com>
2014-03-23 12:43:47 +0000

    GPL3 licensed

--- src/test/java/io/meles/amqp/spring/SnappyCompressorTest.java
+++ src/test/java/io/meles/amqp/spring/SnappyCompressorTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2014 Neil Green
+ *
+ * This file is part of Meles AMQP.
+ *
+ * Meles AMQP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Meles AMQP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Meles AMQP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.meles.amqp.spring;
 
 import static org.hamcrest.Matchers.equalTo;
